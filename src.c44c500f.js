c965ad7893b1aeb8a013af651b844bfd8c68190f
ProtsyukPrits <78557776+ProtsyukPrits@users.noreply.github.com>
2022-05-27 22:53:47 +0000

    Deploying to gh-pages from @ ProtsyukPrits/project@dd0fd4a2ed0d14a4f003fea81035ec7978c65c4f 🚀

--- src.c44c500f.js
+++ src.c44c500f.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project/src.edd8fd12.js.map
+//# sourceMappingURL=/project/src.c44c500f.js.map
