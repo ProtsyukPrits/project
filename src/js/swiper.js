038816fff7dc93db9587dcfc1106f9e24af9388f
VolodymyrVelch <velch83@gmail.com>
2022-05-31 22:02:40 +0300

    half_photo_added

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,22 +2,29 @@ new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+        dynamicBullets: true,
     },
     loop: true,
 
     grabCursor: true,
 
-    speed: 1200,
+    speed: 200,
 
     autoHeight: true,
-    
-    effect: 'flip',
 
-    flipEffect: {
-        slideShadows: true,
-        limitRotation: true
+    initialSlide: 5,
+    
+    // effect: 'flip',
+
+    // flipEffect: {
+    //     slideShadows: true,
+    //     limitRotation: true
         
-    },
+    // },
+    
+    // autoplay: {
+    // delay: 3200,
+    // },
 
     keyboard: {
         enabled: true,
